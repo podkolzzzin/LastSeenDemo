626d7f0622f6a974cee703be871c20e6dff66577
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-10-23 22:59:09 +0300

    Add integrstion and E2E test

--- LastSeenDemo.E2E/Configuration.cs
+++ LastSeenDemo.E2E/Configuration.cs
@@ -2,18 +2,6 @@
 
 public static class Configuration
 {
-    private const string DefaultUrl = "http://127.0.0.1:5079";
-
-    public static string BaseUrl
-    {
-        get
-        {
-            var env = Environment.GetEnvironmentVariable("VM_IP");
-            if (env != null)
-            {
-                return $"http://{env}";
-            }
-            return DefaultUrl;
-        }
-    }
+    public const string
+        BaseUrl = "https://sef.podkolzin.consulting/api/users/lastSeen";
 }
---
+++ LastSeenDemo.IntegrationTest/StatisticsTests.cs
@@ -0,0 +1,13 @@
+﻿using LastSeenDemo.E2E;
+
+namespace LastSeenDemo;
+
+public class StatisticsTests
+{
+    [Fact]
+    public void OverallShouldReturnResult()
+    {
+        var result = HttpHelper.Get("/api/report/statistics");
+        Assert.NotNull(result);
+    }
+}

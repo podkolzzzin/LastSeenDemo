99bb2eec0bb4308c9417eba95761bd2bf04fd583
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2023-10-23 11:01:53 +0300

    Assignment 2 added

---
+++ LastSeenDemo.API/Assignment2.cs
@@ -0,0 +1,242 @@
+﻿using System.Net;
+
+namespace LastSeenDemo;
+
+public class Assignment2
+{
+     
+}
+
+
+
+class Assignment2Features
+{
+    DateTime dtn = DateTime.Now;
+    string now = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffffffK");
+    public void GetLastVisitors(string now)
+    {
+        Dictionary<string, string> users_dict = new Dictionary<string, string>();
+        using (WebClient wc = new WebClient())
+        {
+            var json = wc.DownloadString("https://sef.podkolzin.consulting/api/users/lastSeen?offset=40");
+            string[] s = json.Split('[');
+            string[] subs = s[1].Split('{');
+
+            foreach (var user in subs)
+            {
+                string[] user_info = user.Split(',');
+                string user_str = "";
+                string last_date= "";
+                        
+                foreach (var u in user_info)
+                {
+                    if (u.Contains("nickname"))
+                    {
+                        user_str = u;
+                    }
+                    if (u.Contains("lastSeenDate"))
+                    {
+                        last_date = u;
+                    }
+                }
+                users_dict.Add(user_str, last_date);
+            }
+
+            foreach (var kvp in users_dict)
+            {
+                HumanizeTime(kvp.Key, kvp.Value, now);
+            }
+        }
+    }
+    
+    public void GetLastVisitors2(string now)
+    {
+        Dictionary<string, string> users_dict = new Dictionary<string, string>();
+        int limit = 0;
+        int offset = 40;
+        List<string> all_jsons = new List<string>();
+        while (true)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString($"https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}&limit={limit}");
+                all_jsons.Add(json);
+                if (json.Length < limit)
+                    break;
+                
+                offset += limit;
+                
+                string[] s = json.Split('[');
+                string[] subs = s[1].Split('{');
+
+                foreach (var user in subs)
+                {
+                    string[] user_info = user.Split(',');
+                    string user_str = "";
+                    string last_date= "";
+                        
+                    foreach (var u in user_info)
+                    {
+                        if (u.Contains("nickname"))
+                        {
+                            user_str = u;
+                        }
+                        if (u.Contains("lastSeenDate"))
+                        {
+                            last_date = u;
+                        }
+                    }
+                    users_dict.Add(user_str, last_date);
+                }
+
+                foreach (var kvp in users_dict)
+                {
+                    HumanizeTime(kvp.Key, kvp.Value, now);
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    public string HumanizeTimeOld(Dictionary<string, string> users_dict, string now)
+    {
+        int mounth_now = Int32.Parse(now.Substring(5, 2));
+        int day_now = Int32.Parse(now.Substring(8, 2));
+        int hour_now = Int32.Parse(now.Substring(11, 2));
+        int minute_now = Int32.Parse(now.Substring(14, 2));
+        int sec_now = Int32.Parse(now.Substring(17, 2));
+        foreach (var kvp in users_dict) {
+            var nickname = kvp.Key;
+            if (nickname.Length > 0)
+            {
+                nickname = nickname.Remove(0, 11);
+                Console.Write(nickname);
+            }
+
+            var time_online = kvp.Value;
+            if (time_online.Length > 0 && (!time_online.Contains("null")))
+            {
+                time_online = time_online.Substring(0, time_online.Length-1);
+                time_online = time_online.Remove(0, 16);
+                
+                int mounth_curr = Int32.Parse(time_online.Substring(5, 2));
+                int day_curr = Int32.Parse(time_online.Substring(8, 2));
+                int hour_curr = Int32.Parse(time_online.Substring(11, 2));
+                int minute_curr = Int32.Parse(time_online.Substring(14, 2));
+                int sec_curr = Int32.Parse(time_online.Substring(17, 2));
+
+                if ((sec_now-sec_curr < 30) && minute_curr == minute_now && day_curr == day_now && mounth_curr == mounth_now)
+                {
+                    time_online = " was online jsut now";
+                }
+                else if (minute_now-minute_curr <= 2 && hour_now == hour_curr && day_curr == day_now && mounth_curr == mounth_now)
+                {
+                    time_online = " was online a minute ago";
+                }
+                else if (hour_now-hour_curr <= 1 && day_curr == day_now && mounth_curr == mounth_now)
+                {
+                    time_online = " was online a few minutes ago";
+                }
+                else if (hour_now-hour_curr <= 2 && day_curr == day_now && mounth_curr == mounth_now)
+                {
+                    time_online = " was online an hour ago";
+                }
+                else if (day_now == day_curr && mounth_curr == mounth_now)
+                {
+                    time_online = " was online today";
+                }
+                else if (day_now-day_curr <=2 && mounth_curr == mounth_now)
+                {
+                    time_online = " was online yesterday";
+                }
+                else if (day_now-day_curr <=8 && mounth_curr == mounth_now)
+                {
+                    time_online = " was online this week";
+                } 
+                else
+                {
+                    time_online = " was online long ago";
+                }
+            }
+            else if(time_online.Contains("null"))
+            {
+                time_online = " is online";
+            }
+            Console.WriteLine(time_online);
+        }
+
+        return "";
+    }
+    
+    public string HumanizeTime(string user_name, string user_time, string now)
+    {
+        int mounth_now = Int32.Parse(now.Substring(5, 2));
+        int day_now = Int32.Parse(now.Substring(8, 2));
+        int hour_now = Int32.Parse(now.Substring(11, 2));
+        int minute_now = Int32.Parse(now.Substring(14, 2));
+        int sec_now = Int32.Parse(now.Substring(17, 2));
+         var nickname = user_name;
+        if (nickname.Length > 0)
+        {
+            nickname = nickname.Remove(0, 11);
+            Console.Write(nickname);
+        }
+
+        var time_online = user_time;
+        if (time_online.Length > 0 && (!time_online.Contains("null")))
+        {
+            time_online = time_online.Substring(0, time_online.Length-1);
+            time_online = time_online.Remove(0, 16);
+            
+            int mounth_curr = Int32.Parse(time_online.Substring(5, 2));
+            int day_curr = Int32.Parse(time_online.Substring(8, 2));
+            int hour_curr = Int32.Parse(time_online.Substring(11, 2));
+            int minute_curr = Int32.Parse(time_online.Substring(14, 2));
+            int sec_curr = Int32.Parse(time_online.Substring(17, 2));
+
+            if ((sec_now-sec_curr < 30) && minute_curr == minute_now && day_curr == day_now && mounth_curr == mounth_now)
+            {
+                return " was online jsut now";
+            }
+            else if (minute_now-minute_curr <= 2 && hour_now == hour_curr && day_curr == day_now && mounth_curr == mounth_now)
+            {
+                return " was online a minute ago";
+            }
+            else if (hour_now-hour_curr < 1 && day_curr == day_now && mounth_curr == mounth_now)
+            {
+                return " was online a few minutes ago";
+            }
+            else if (hour_now-hour_curr <= 2 && day_curr == day_now && mounth_curr == mounth_now)
+            {
+                return " was online an hour ago";
+            }
+            else if (day_now == day_curr && mounth_curr == mounth_now)
+            {
+                return " was online today";
+            }
+            else if (day_now-day_curr <=2 && mounth_curr == mounth_now)
+            {
+                return " was online yesterday";
+            }
+            else if (day_now-day_curr <=8 && mounth_curr == mounth_now)
+            {
+                return " was online this week";
+            } 
+            else
+            {
+                return " was online long ago";
+            }
+        }
+        else if (time_online.Contains("null"))
+        {
+            return " is online";
+        }
+        else
+        {
+            return " unavailable";
+        }
+    }
+}
---
+++ LastSeenDemo.API/Assignment2testing.cs
@@ -0,0 +1,73 @@
+﻿using Xunit;
+
+namespace LastSeenDemo;
+
+
+public class ToBeTestedCoverTests2ElectricBoogaloo
+{
+    [Fact]
+    public void JustNowScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-09-22T06:45:36.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online jsut now", few_sec_ago);          // 20 sec difference
+    }
+    [Fact]
+    public void AMinAgoScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-09-22T06:44:46.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online a minute ago", few_sec_ago);          //  1 min 10 sec difference
+    }
+    [Fact]
+    public void FewMinAgoScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-09-22T06:25:56.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online a few minutes ago", few_sec_ago);          // 20 min difference
+    }
+    [Fact]
+    public void AnHourAgoScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-09-22T05:43:56.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online an hour ago", few_sec_ago);          // 1 hour 2 min difference
+    }
+    [Fact]
+    public void AnTodayScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-09-22T01:23:56.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online today", few_sec_ago);          // 5 hours 12 min difference
+    }
+    [Fact]
+    public void YesterdayScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-09-21T05:45:56.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online yesterday", few_sec_ago);          // 25 hours difference
+    }
+
+    [Fact]
+    public void ThisWeekScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean",
+            "abcdefghijklmnop2023-09-18T06:45:56.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online this week", few_sec_ago); // 4 days difference
+    }
+    [Fact]
+    public void LongAgoScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var few_sec_ago = program.HumanizeTime("31545374634mr Bean", "abcdefghijklmnop2023-02-22T06:45:56.5192279+00:00", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" was online long ago", few_sec_ago);          // 2 months difference
+    }
+    [Fact]
+    public void OnlineScenario()
+    {
+        Assignment2Features program = new Assignment2Features();
+        var when_last_online = program.HumanizeTime("31545374634mr Bean", "null", "2023-09-22T06:45:56.5192279+00:00");
+        Assert.Equal(" is online", when_last_online);          //
+    }
+}
